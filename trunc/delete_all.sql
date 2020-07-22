0b307461aa11af382c05b4547ee501014bcc79db
Xalo-dev <54641400+Xalo-dev@users.noreply.github.com>
2020-07-22 16:06:10 +0300

    First single decay update

---
+++ trunc/delete_all.sql
@@ -0,0 +1,6 @@
+-- Deletes all the tables in the database (for development purposes)
+
+DROP TABLE single_decay;
+DROP TABLE other_transition;
+DROP TABLE ncalc;
+DROP TABLE nucleus;
--- trunc/sd_data.sql
+++ trunc/sd_data.sql
@@ -4,4 +4,5 @@
 
 -- Symbol, calculation ID, Ei, Ef, JPi_i, JPi_f,k_i,k_f,Branching Ratio (%),
 -- decay type, file reference, forbiddeness
-INSERT INTO single_decay VALUES ('39Ar',1,0,0,'7/2-','3/2+',1,1,100,'B-',NULL,'1FU');
+INSERT INTO single_decay VALUES ('39Ar',1,0,0,'7/2-','3/2+',1,1,100,'B-','39Ar-sdpfu','1FU');
+INSERT INTO single_decay VALUES ('39Ar',2,0,0,'7/2-','3/2+',1,1,100,'B-','39Ar-sdpfk','1FU');

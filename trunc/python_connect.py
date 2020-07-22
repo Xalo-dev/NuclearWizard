86c845c5f808912bff1e721c687b0bc600b451f9
Xalo-dev <54641400+Xalo-dev@users.noreply.github.com>
2020-07-22 23:08:48 +0300

    Update python_connect.py

--- trunc/python_connect.py
+++ trunc/python_connect.py
@@ -1,14 +1,23 @@
 # -*- coding: utf-8 -*-
 """
-Initialize database connection nuclear_wizard
+Initialize database connection to nuclear_wizard
 """
 
 import mysql.connector
 
 mydb = mysql.connector.connect(
         host="localhost",
-        user="root",
-        password="root"
+        user="theorist",
+        password="theorist",
+        database="nuclear_wizard"
     )
 
-print(mydb)
+mycursor = mydb.cursor()
+
+nucleusInsert ="INSERT INTO nucleus VALUES (%s, %s, %s, %s, %s, %s, %s)"
+nuclei = [('205Tl',205,81,None,None,None,'1/2+'),
+          ('205Pb',205,82,1.73,7,'Y','5/2-')]
+
+mycursor.executemany(nucleusInsert, nuclei)
+
+mydb.commit()
